3c07124f71cd318c856d7172115dc69cb543bdd0
hyunjin <leehj0110@kakao.com>
2022-05-21 11:06:25 +0900

    feat: typing Log, LogContext

--- contexts/LogContext.tsx
+++ contexts/LogContext.tsx
@@ -1,22 +1,39 @@
 import React, {createContext, useContext, useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
+export interface Log {
+  id: string;
+  title: string;
+  body: string;
+  date: Date;
+}
 export interface LogContextValue {
-  text: string;
-  // setText: (text: string) => void;
-  setText: React.Dispatch<React.SetStateAction<string>>;
+  logs: Log[];
+  onCreate: ({title: body, date}: Partial<Log>) => void;
 }
 
 const LogContext = createContext<LogContextValue | null>(null);
 
 export function LogContextProvider({children}: {children: React.ReactNode}) {
-  const [text, setText] = useState('');
+  const [logs, setLogs] = useState<Log[]>([]);
+
+  const onCreate = ({title, body, date}: Partial<Log>) => {
+    const log = {
+      id: uuidv4(),
+      title,
+      body,
+      date,
+    } as Log;
+    setLogs([log, ...logs]);
+  };
 
   return (
-    <LogContext.Provider value={{text, setText}}>
+    <LogContext.Provider value={{logs, onCreate}}>
       {children}
     </LogContext.Provider>
   );
 }
+
 // Context를 만들면 LogContext.Provider와 LogContext.Consumer가 생김
 // Provider는 Context안에 있는 값을 사용할 컴포넌트들을 감싸는 용도
 
